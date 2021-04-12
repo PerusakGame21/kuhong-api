5cb958830206b75f2d94a3fd0ea81ea59c3f0ed8
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2021-04-12 16:25:37 +0700

    Update invalid_apikey.js

--- invalid_apikey.js
+++ invalid_apikey.js
@@ -1,4 +1,4 @@
 {
   "creator": "RC047",
-  "erorr": "Apikey Invalid!\n\nSilahkan hubungi owner untuk beli apikey baru :)"
+  "forbidden": "Apikey Invalid!\n\nSilahkan hubungi owner untuk beli apikey baru :)"
 }
